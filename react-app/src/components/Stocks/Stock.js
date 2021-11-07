1cd6cf1d8696a88414ebcade8a491da0c641fca0
AJ Abushaban <amjad.abushaban@yahoo.com>
2021-11-07 10:27:53 -0600

    begin styling news feed

--- react-app/src/components/Stocks/Stock.js
+++ react-app/src/components/Stocks/Stock.js
@@ -4,42 +4,44 @@ import { useParams } from "react-router-dom";
 import { getStock } from "../../store/stocks";
 import StockNews from "./StockNews";
 function Stock() {
-	const { stockId } = useParams();
-    const stock = useSelector((state) => state.stocks.stock);
-    const dispatch = useDispatch();
-	useEffect(() => {
-		if (!stockId) {
-			return;
-		}
-		(async () => {
-            await dispatch(getStock(stockId));
-		})();
-	}, [stockId]);
+  const { stockId } = useParams();
+  const stock = useSelector(state => state.stocks.stock);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!stockId) {
+      return;
+    }
+    (async () => {
+      await dispatch(getStock(stockId));
+    })();
+  }, [stockId]);
 
-	if (!stock) {
-		return null;
-	}
+  if (!stock) {
+    return null;
+  }
 
-	return (
-		<div id="main-stock-div">
-		<ul>
-			<li>
-				<strong>Stock Name</strong> {stock["companyName"]}
-			</li>
-			<li>
-				<strong>Latest Price</strong> {stock["latestPrice"]}
-			</li>
-			<li>
-				<strong>Market Cap</strong> {stock["marketCap"]}
-			</li>
-		</ul>
-		<div>
-			<h1 style={{textAlign:"center"}}>Recent News</h1>
-			{stock["news"].map(article => {
-				return <StockNews key={article.id} news={article} />
-			})}
-		</div>
-		</div>
-	);
+  return (
+    <div id="main-stock-div">
+      <ul id="stock-info">
+        <li>
+          <strong>Stock Name</strong> {stock["companyName"]}
+        </li>
+        <li>
+          <strong>Latest Price</strong> {stock["latestPrice"]}
+        </li>
+        <li>
+          <strong>Market Cap</strong> {stock["marketCap"]}
+        </li>
+      </ul>
+      <div id="news-container">
+        <h1 style={{ textAlign: "center", marginBottom: "50px" }}>
+          Recent News
+        </h1>
+        {stock["news"].map(article => {
+          return <StockNews key={article.id} news={article} />;
+        })}
+      </div>
+    </div>
+  );
 }
 export default Stock;
