e3dcb50decf7a55edbfb93ffa552b6cc2cfcca8d
nick <nicolasjsim@gmail.com>
2021-11-09 09:21:05 -0800

    fixed console errors

--- react-app/src/components/Lists/List.js
+++ react-app/src/components/Lists/List.js
@@ -1,15 +1,15 @@
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 
 
 function List(){
-    const sessionUser = useSelector(state => state.session.user);
+    //const sessionUser = useSelector(state => state.session.user);
 
     const testLists = [{"id": 1, "name": "First List", "user_id": 1},{"id": 2, "name": "Second List", "user_id": 1}]
 
     return(
         <div className="allLists">
             {testLists.map(list => (
-                <li key={list.user_id}>{list.name}</li>
+                <li key={list.id}>{list.name}</li>
             ))}
 
 
